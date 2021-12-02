1a60607a15a98619b1cd5923c3ed8395fadcb54e
a-malmberg <amalmberg@my.waketech.edu>
2021-12-02 17:55:06 -0500

    add number test

---
+++ numbers_test.pl
@@ -0,0 +1,10 @@
+use warnings;
+use strict;
+use 5.010;
+
+say 255; 	#regular base 10
+say 0377;	#octal
+say 0b11111111; #binary
+say 0xFF;	#hex
+
+# 0 before means octal, 0b means binary, 0x means hex
