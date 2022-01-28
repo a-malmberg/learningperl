9c5c7cabca6729441dddea9d64a6ccbb46f448b5
a-malmberg <amalmberg@my.waketech.edu>
2022-01-27 20:52:36 -0500

    ch3ex1

---
+++ Ch3Ex1.pl
@@ -0,0 +1,12 @@
+#!perl
+use strict;
+use warnings;
+use v5.14;
+use utf8;
+
+#say 'Enter some lines, then hit Ctrl-D';
+#my @lines = <STDIN>;
+#say reverse @lines;
+
+say 'Another alternative:';
+say reverse <STDIN>;
